4a8a54b03e2b8571d1bffd48a2ec7b4d52704353
Sebastian Abarca <49495659+abrusebas1997@users.noreply.github.com>
2021-03-19 01:51:18 -0400

    Update urls.py

--- AppointmentScheduler/AppointmentScheduler/urls.py
+++ AppointmentScheduler/AppointmentScheduler/urls.py
@@ -23,4 +23,6 @@ urlpatterns = [
  path('admin/', admin.site.urls),
  path('', include("App.urls")),
 
-]  
+]
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
