980bdaeb5143293f395ec34926c1cd93ad1b06bf
Sebastian Abarca <49495659+abrusebas1997@users.noreply.github.com>
2021-03-19 01:23:49 -0400

    added [secret key]
    Follow the Fauna API instructions to make collections and indexes in order to create a database

--- AppointmentScheduler/App/views.py
+++ AppointmentScheduler/App/views.py
@@ -2,7 +2,7 @@ from faunadb import query as q
 from faunadb.objects import Ref
 from faunadb.client import FaunaClient
 
-client = FaunaClient(secret="your-secret-here")
+client = FaunaClient(secret="fnAEEpXoUEACANrA9kCkDGRnaTM0De2OHmQMFgVS")
 
 indexes = client.query(q.paginate(q.indexes()))
 
