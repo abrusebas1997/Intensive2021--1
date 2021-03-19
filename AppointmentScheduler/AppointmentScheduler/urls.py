71aacd5f4734d639db8173745e2653e292ce7b58
Sebastian Abarca <49495659+abrusebas1997@users.noreply.github.com>
2021-03-19 01:43:29 -0400

    Update urls.py

--- AppointmentScheduler/AppointmentScheduler/urls.py
+++ AppointmentScheduler/AppointmentScheduler/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.contrib.staticfiles.urls import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+ path('admin/', admin.site.urls),
+ path('', include("App.urls")),
+
+]  
