ce457677545c3e5ff0c723b6316b114b09a8f104
Sebastian Abarca <49495659+abrusebas1997@users.noreply.github.com>
2021-03-19 01:43:59 -0400

    Update views.py

--- AppointmentScheduler/App/views.py
+++ AppointmentScheduler/App/views.py
@@ -1,7 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib import messages
+from django.core.paginator import Paginator
+from django.http import HttpResponseNotFound
 from faunadb import query as q
+import pytz
 from faunadb.objects import Ref
 from faunadb.client import FaunaClient
+import hashlib
+import datetime
+
 
 client = FaunaClient(secret="fnAEEpXoUEACANrA9kCkDGRnaTM0De2OHmQMFgVS")
 
