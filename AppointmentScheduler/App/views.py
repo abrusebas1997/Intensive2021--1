97da6cb7d92fc749f01079c2248cb192c2e601b6
Sebastian Abarca <49495659+abrusebas1997@users.noreply.github.com>
2021-03-19 01:42:58 -0400

    Update views.py

--- AppointmentScheduler/App/views.py
+++ AppointmentScheduler/App/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import render
 from faunadb import query as q
 from faunadb.objects import Ref
 from faunadb.client import FaunaClient
@@ -6,4 +7,24 @@ client = FaunaClient(secret="fnAEEpXoUEACANrA9kCkDGRnaTM0De2OHmQMFgVS")
 
 indexes = client.query(q.paginate(q.indexes()))
 
-print(indexes)
+def login(request):
+ return render(request,"login.html")
+
+def create_appointment(request):
+ return render(request,"appoint/create-appointment.html")
+
+
+
+def dashboard(request):
+ return render(request,"index.html")
+
+
+def today_appointment(request):
+ return render(request,"today-appointment.html")
+
+
+def all_appointment(request):
+ return render(request,"all-appointment.html")
+
+def register(request):
+ return render(request,"register.html")
